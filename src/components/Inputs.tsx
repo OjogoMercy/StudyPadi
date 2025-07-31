322d470445682c87b0b9c516fe0e2ef9cd798532
Armykookie35 <mercyojogo@gmail.com>
2025-07-31 14:41:26 +0100

    Added Components

---
+++ src/components/Inputs.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Inputs = () => {
+  return (
+    <View>
+      <Text>Inputs</Text>
+    </View>
+  )
+}
+
+export default Inputs
+
+const styles = StyleSheet.create({})

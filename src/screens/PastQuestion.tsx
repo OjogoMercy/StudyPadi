713dc45a3ac7271de0a7416a6dc8c737a7664e15
Armykookie35 <mercyojogo@gmail.com>
2025-07-31 15:32:39 +0100

    Updated Screens...

---
+++ src/screens/PastQuestion.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PastQuestion = () => {
+  return (
+    <View>
+      <Text>PastQuestion</Text>
+    </View>
+  )
+}
+
+export default PastQuestion
+
+const styles = StyleSheet.create({})

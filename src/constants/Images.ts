cde8284962d1572b3ee623b0cc73ceab6629df22
Armykookie35 <mercyojogo@gmail.com>
2025-07-31 16:20:50 +0100

    updated Structure...

---
+++ src/constants/Images.ts
@@ -0,0 +1,9 @@
+export const Images = {
+  avatar: require("../assets/images/avatar.png"),
+    avatar1: require("../assets/images/avatar1.jpg"),
+    avatar2: require("../assets/images/avatar2.jpg"),
+    avatar3: require("../assets/images/avatar3.jpg"),
+    avatar4: require("../assets/images/avatar4.jpg"),
+    avatar5: require("../assets/images/avatar5.jpg"),
+    
+};

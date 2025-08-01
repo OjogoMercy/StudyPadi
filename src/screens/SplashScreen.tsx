39d9445513ab4cf4d78692382b879db3ad77b68a
Armykookie35 <mercyojogo@gmail.com>
2025-08-01 14:58:32 +0100

    Added animation to the splashscreen...

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,13 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Colors ,FONTS, SCREEN_HEIGHT} from '../constants/Theme'
+import { Image, StyleSheet, Text, View,Animated } from 'react-native'
+import React, { useEffect, useRef } from 'react'
+import { Colors ,FONTS, SCREEN_HEIGHT, SCREEN_WIDTH} from '../constants/Theme'
+import { Images } from '../constants/Images'
 
 const SplashScreen = () => {
+const imageOpacity = useRef(new Animated.Value(0)).current;
+const textOpacity = useRef(new Animated.Value(0)).current;
+const textTranslateY = useRef(new Animated.Value(50)).current;
+
+
+      useEffect(() => {
+    // Fade in image
+    Animated.timing(imageOpacity, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true,
+    }).start(() => {
+      // After image fades, slide in text
+     Animated.parallel([
+       Animated.timing(textTranslateY, {
+         toValue: 0,
+         duration: 3000,
+         useNativeDriver: true,
+       }),
+       Animated.timing(textOpacity, {
+         toValue: 1,
+         duration: 3000,
+         useNativeDriver: true,
+       }),
+     ]).start();
+    });
+  }, []);
   return (
-    <View style={{backgroundColor:Colors.primary,flex:1,justifyContent:'center',alignItems:'center'}}>
-      <Text style={styles.header}>StudyPadi</Text>
+    <View
+      style={{
+        backgroundColor: Colors.primary,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Animated.Image
+          source={[Images.logo, { opacity: imageOpacity }]}
+          style={styles.logo}
+        />
+        <Animated.Text
+          style={[
+            styles.header,
+            { transform: [{ translateY: textTranslateY }] },
+          ]}
+        >
+          tudyPadi
+        </Animated.Text>
+      </View>
     </View>
-  )
+  );
 }
 
 export default SplashScreen
@@ -16,7 +64,12 @@ const styles = StyleSheet.create({
   header: {
     ...FONTS.h1,
     color: Colors.white,
-    textAlign: 'center',
-    marginTop: SCREEN_HEIGHT* 0.1,
-  }
-})
+    textAlign: "center",
+
+  },
+  logo: {
+    width: SCREEN_WIDTH * 0.25,
+    height: SCREEN_HEIGHT * 0.15,
+    tintColor: Colors.white,
+  },
+});

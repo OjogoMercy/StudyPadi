e1cc0423247581d5a11012ecbc65fc12896aa8af
Armykookie35 <mercyojogo@gmail.com>
2025-08-14 21:23:28 +0100

    ...

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, TouchableOpacity, View, Image, ScrollView } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Image, ScrollView,FlatList } from "react-native";
 import React, { useState } from "react";
 import general from "../constants/General";
-import { Colors, FONTS, SCREEN_HEIGHT, SCREEN_WIDTH } from "../constants/Theme";
+import { Colors, FONTS, SCREEN_HEIGHT, SCREEN_WIDTH ,Sizes} from "../constants/Theme";
 import { useNavigation } from "expo-router";
 import { Images } from "../constants/Images";
 import Ionicons from "@expo/vector-icons/Ionicons";
@@ -13,7 +13,19 @@ const Profile = () => {
   return (
     <ScrollView style={general.container}>
       <CustomHeader title={'Profile'} />
-      
+      <View>
+        <FlatList
+          data={menuOptions}
+          renderItem={({ item }) => {
+            return (
+              <View style={{flexDirection:'row',padding:Sizes.smallPadding}}>
+                <Ionicons name={item.icon} size={Sizes.body4} />
+                <Text style={{...FONTS.body4}}>{ item.title}</Text>
+              </View>
+            )
+          }}
+        />
+      </View>
     </ScrollView>
   );
 };

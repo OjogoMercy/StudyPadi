381e568cacf67b3567ccb0d1b09619a6e9ee5baf
Armykookie35 <mercyojogo@gmail.com>
2025-08-21 22:54:09 +0100

    ...

--- src/screens/PastQuestion.tsx
+++ src/screens/PastQuestion.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import {  StyleSheet,  Text,  TouchableOpacity, View,  Image,  FlatList,} from "react-native";
+import React, { useState } from "react";
+import general from "../constants/General";
+import {
+  Colors,
+  FONTS,
+  SCREEN_HEIGHT,
+  SCREEN_WIDTH,
+  Sizes,
+} from "../constants/Theme";
+import { useNavigation } from "expo-router";
+import { Images } from "../constants/Images";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import CustomButton from "../components/CustomButton";
+import CustomInput from "../components/CustomInput";
 const PastQuestion = () => {
+  const [search,setSearch] = useState('')
   return (
-    <View>
-      <Text>PastQuestion</Text>
+    <View style={general.container}>
+      <View style={[general.row, {alignItems:'center'}]}>
+        <Text style={{ ...FONTS.h2, color: Colors.primary }}>
+          Past Questions
+        </Text>
+          <CustomButton title="Request" />
+      </View>
+      <CustomInput value={search} onChangeText={setSearch} iconName="search" placeholder="Search for past questions..." containerStyle={{marginTop:Sizes.body1}}/>
     </View>
-  )
-}
+  );
+};
 
-export default PastQuestion
+export default PastQuestion;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
